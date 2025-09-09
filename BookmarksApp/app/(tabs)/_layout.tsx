20ba369992d3e4f12c9f74bf4892e22c067b2795
kenf1 <103538771+kenf1@users.noreply.github.com>
2025-09-09 18:01:42 +0000

    Add tabs

---
+++ BookmarksApp/app/(tabs)/_layout.tsx
@@ -0,0 +1,37 @@
+import { Tabs } from "expo-router";
+
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "coral" }}>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, focused }) => {
+            return focused ? (
+              <FontAwesome name="home" size={24} color={color} />
+            ) : (
+              <FontAwesome name="home" size={24} color="gray" />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, focused }) => {
+            return focused ? (
+              <Ionicons name="settings-sharp" size={24} color={color} />
+            ) : (
+              <Ionicons name="settings-sharp" size={24} color="gray" />
+            );
+          },
+        }}
+      />
+    </Tabs>
+  );
+}
--- BookmarksApp/app/index.tsx
+++ BookmarksApp/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -10,6 +11,8 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      <Link href={"/home"}>Home page</Link>
     </View>
   );
 }
